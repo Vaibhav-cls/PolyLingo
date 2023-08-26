c7b3f74f963a3b30aa85296b89071ce21d1ecba1
Vaibhav-cls <63966372+Vaibhav-cls@users.noreply.github.com>
2023-08-26 15:03:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener('scroll',reveal);
+window.addEventListener('scroll',reveali);
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i=0;i<reveals.length;i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 50;
+
+        if(revealtop<windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+function reveali(){
+    var reveals = document.querySelectorAll('.revealio');
+
+    for(var i=0;i<reveals.length;i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 200;
+
+        if(revealtop<windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
